c164a21f7d4ec36dd11345a247c713e7ff59e500
fernandongana <fernandongana@gmail.com>
2021-09-20 17:59:01 +0200

    update login view model

--- app/src/main/java/co/mz/noteApp/PerfilFragment.kt
+++ app/src/main/java/co/mz/noteApp/PerfilFragment.kt
@@ -1,5 +1,6 @@
 package co.mz.noteApp
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -13,6 +14,10 @@ import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import co.mz.noteApp.databinding.FragmentPerfilBinding
 import co.mz.noteApp.viewmodel.UserViewModel
+import android.content.SharedPreferences
+
+
+
 
 class PerfilFragment : Fragment() {
 
@@ -29,12 +34,13 @@ class PerfilFragment : Fragment() {
     private fun init(){
         val navController = findNavController()
         userViewModel.user.observe(viewLifecycleOwner, { user ->
-            if (user != null) {
+            if (user?.email != null) {
+                Log.v("init", user.email.toString())
                 showWelcomeMessage(user.email.toString())
                 Log.v("user Profile", "Already have User......")
             } else {
                 Log.v("user Profile", "Navigate to Login......")
-
+                showWelcomeMessage("Login Failed")
                 navController.navigate(R.id.loginFragment)
             }
         })
@@ -49,30 +55,43 @@ class PerfilFragment : Fragment() {
         userViewModel = activity?.run {
             ViewModelProvider(this).get(UserViewModel::class.java)
         } ?: throw Exception("Invalid Activity")
-       // userViewModel.login("gawat@gmail.com","123456")
-        userViewModel.login("x","x")
+
         val navController = findNavController()
 
-        val currentBackStackEntry = navController.currentBackStackEntry!!
+        val preferences = requireActivity().getSharedPreferences("MY_APP", Context.MODE_PRIVATE)
+        val retrivedToken = preferences.getString("TOKEN", null) //second parameter default value.
+
+        if(retrivedToken.isNullOrEmpty()){
+            userViewModel.initiate()
+        }else{
+          //  userViewModel.login("fernandongana@gmail.com","123456")
+            userViewModel.link(retrivedToken)
+        }
+
+
+
+
+   /*     val currentBackStackEntry = navController.currentBackStackEntry!!
         val savedStateHandle = currentBackStackEntry.savedStateHandle
         savedStateHandle.getLiveData<Boolean>(LoginFragment.LOGIN_SUCCESSFUL)
             .observe(currentBackStackEntry, { success ->
                 if (!success) {
+                    Log.v("Login", "LOGIN_SUCCESSFUL")
                     val startDestination = navController.graph.startDestination
                     val navOptions = NavOptions.Builder()
                         .setPopUpTo(startDestination, true)
                         .build()
                     navController.navigate(startDestination, null, navOptions)
+                }else{
+                    Log.v("Login", "Login Failed")
                 }
-            })
+            }) */
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
         // Inflate the layout for this fragment
         _binding = FragmentPerfilBinding.inflate(inflater,container,false)
         return  binding.root
--- app/src/main/java/co/mz/noteApp/viewmodel/UserViewModel.kt
+++ app/src/main/java/co/mz/noteApp/viewmodel/UserViewModel.kt
@@ -1,7 +1,9 @@
 package co.mz.noteApp.viewmodel
 
 import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -35,6 +37,30 @@ class UserViewModel : ViewModel(){
         return user
     }
 
+    fun link(token: String){
+        val credential = GoogleAuthProvider.getCredential(token, null)
+        auth.currentUser!!.linkWithCredential(credential)
+            .addOnCompleteListener{ task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "linkWithCredential:success")
+                    val user = task.result?.user
+                    Log.d(TAG, user?.email.toString())
+                } else {
+                    Log.w(TAG, "linkWithCredential:failure", task.exception)
+
+                }
+            }
+    }
+
+    fun logOut(){
+        auth.signOut()
+        user.value = null
+    }
+
+    fun initiate(){
+        user.value = null
+    }
+
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
@@ -42,12 +68,12 @@ class UserViewModel : ViewModel(){
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(ContentValues.TAG, "signInWithCredential:success")
-                    val user = auth.currentUser
-                    //   updateUI(user)
+                    user.value = task.result?.user
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(ContentValues.TAG, "signInWithCredential:failure", task.exception)
-                    //    updateUI(null)
+                    user.value = null
+                    _result.value = task.exception
                 }
             }
     }
